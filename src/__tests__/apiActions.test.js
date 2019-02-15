e6d755a1de5a3bc79bab0a8d9e2f1bf9673ae32a
Safarali <mr.safarali@gmail.com>
2019-02-14 16:51:37 -0800

    Test apiActions action creators

---
+++ src/__tests__/apiActions.test.js
@@ -0,0 +1,37 @@
+import * as actions from '../actions/apiActions';
+
+describe("apiStart", () => {
+    test("has type API_START", () => {
+        const expectedVal = {
+            type: "API_START"
+        };
+        expect(actions.apiStart()).toEqual(expectedVal);
+    });
+
+    test("has type API_START and payload", () => {
+        const expectedVal = {
+            type: "API_START",
+            payload: 22
+        };
+        expect(actions.apiStart(22)).toEqual(expectedVal);
+    });
+});
+
+describe("apiEnd", () => {
+    test("has type API_END", () => {
+        const expectedVal = {
+            type: "API_END",
+        };
+        expect(actions.apiEnd()).toEqual(expectedVal);
+    });
+
+    test("has type API_END and payload", () => {
+        const expectVal = {
+            type: "API_END",
+            payload: 22
+        };
+
+        expect(actions.apiEnd(22)).toEqual(expectVal);
+    })
+})
+
